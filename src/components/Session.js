9248690661c69512c739fbbffa4664d9b884a7ab
rajan <rajyotiroshan@gmail.com>
2023-04-12 23:50:12 +0530

    session

--- src/components/Session.js
+++ src/components/Session.js
@@ -9,7 +9,14 @@ export default function Session() {
 
   return (
     <>
+      <h3>{name}</h3>
+      <p>{desc}</p>
 
+      <h4>{speaker.name}</h4>
+      <span>
+        {speaker.title} at {speaker.org}
+      </span>
+      <p>{speaker.bio}</p>
     </>
   );
 }
