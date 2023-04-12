f04fdcfebd1bfb5f32af7b703cb8fa7b1f23df49
rajan <rajyotiroshan@gmail.com>
2023-04-12 15:18:38 +0530

    basic routes and route

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,15 @@
+import { getCategories } from "../api";
+
 export default function Categories() {
+  const categories = getCategories();
   return (
     <div className="container">
       <h1>Session Categories</h1>
+      <ul className="categories">
+        {categories.map((cat) => {
+          return <li key={cat.key}>{cat.name}</li>;
+        })}
+      </ul>
     </div>
   );
 }
