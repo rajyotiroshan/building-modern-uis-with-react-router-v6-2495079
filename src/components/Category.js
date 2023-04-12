332c53babaf7682b96bc2e414f6f7ce6f06dc473
rajan <rajyotiroshan@gmail.com>
2023-04-12 19:37:43 +0530

    indivisual session

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,8 +1,23 @@
+import { useParams } from "react-router";
+import { getCategory } from "../api";
+
 export default function Category() {
+  const { catid } = useParams();
+  const category = getCategory(catid);
+  console.log(category);
   return (
     <>
-      <h2>Category Sessions</h2>
-      <p>Category session content displays here...</p>
+      <h2>{category.name} Sessions</h2>
+      <ul className="session-list">
+        {category?.sessions.map((session) => (
+          <li className="session">
+            <p className="session-name">{session.name}</p>
+            <p>
+              {session.speaker.name} | {session.speaker.org}
+            </p>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
